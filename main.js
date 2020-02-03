b45d01f40f8bb63fe9dd0081eb22c5cb28e68e31
Marius Burkard <m.burkard@pixcept.de>
2020-02-03 11:32:25 +0100

    - changed patch version info

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const packageJson = require('./package.json');
 const adapterName = packageJson.name.split('.').pop();
 const adapterVersion = packageJson.version;
 
-const patchVersion = '';
+const patchVersion = '.3';
 
 let adapter;
 var deviceUsername;
